92125f0b4ccee793d9dd6019ff4e06c037e289f5
Ian Price <michael.ian.price@gmail.com>
2017-03-21 10:05:00 -0400

    Python port using pyquery working.

--- python/grabber.py
+++ python/grabber.py
@@ -1,1 +1,19 @@
-# Port grabber.js to python script for use on ihmga site
+from pyquery import PyQuery as pq
+
+base_url = "http://m.ghin.com/HLR.aspx?ghinno="
+ghin_nums = ['2064561','2259440','0878514']
+result = {}
+
+for ghin in ghin_nums:
+    d = pq(url = base_url + ghin)
+    h = d("#ctl00_cphContent_lblHI").text();
+    n = d("#ctl00_cphContent_lblName").text();
+    c = d("#ctl00_cphContent_lblClub").text();
+    result[ghin] = {
+        'name': n,
+        'handicap': h,
+        'club': c
+    }
+
+for k in result.keys():
+    print "GHIN: %s\n%r\n" % (k, result[k])
